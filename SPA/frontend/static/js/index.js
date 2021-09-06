15a070ac6c7dd69afa76f467796ed0cb71f60455
SangMin-Code <ragdexplorer@gmail.com>
2021-09-06 10:36:07 +0900

    client js navigate with history

--- SPA/frontend/static/js/index.js
+++ SPA/frontend/static/js/index.js
@@ -1,3 +1,8 @@
+const navigateTo = (url) => {
+	history.pushState(null, null, url);
+	router();
+};
+
 const router = async () => {
 	const routes = [
 		{ path: '/', view: () => console.log('Viewing DashBoard') },
@@ -27,6 +32,14 @@ const router = async () => {
 	console.log(match.route.view);
 };
 
+window.addEventListener('popstate', router);
+
 window.onload = () => {
+	document.body.addEventListener('click', (e) => {
+		if (e.target.matches('[data-link]')) {
+			e.preventDefault();
+			navigateTo(e.target.href);
+		}
+	});
 	router();
 };
